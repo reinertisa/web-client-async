8379afc21c382981b5e718d1a2f940afd1b2226e
Isa Reinert <reinert.isa@gmail.com>
2025-06-12 13:40:39 -0700

    feat(api): initialize project

---
+++ src/test/java/com/reinertisa/webclientasync/WebClientAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.webclientasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
